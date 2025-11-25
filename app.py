c3c42b2b35f16198a344730f9ef4ea2bf44300db
Tayyab <tayyabaftab614@gmail.com>
2025-11-25 19:35:39 +0500

    Initial commit with Railway deployment setup

--- app.py
+++ app.py
@@ -191,4 +191,5 @@ def create_tables():
 
 if __name__ == '__main__':
     create_tables()
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
